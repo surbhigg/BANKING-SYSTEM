2d9ed37ff4fccde37b91e9f2584ef78fcc75232d
surbhigg <67185440+surbhigg@users.noreply.github.com>
2021-03-21 15:51:08 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,30 @@
+<section>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    	<footer class="page-footer grey darken-4">
+          <div class="container">
+            <div class="row">
+              <div class="col l6 s12">
+                <h5 class="white-text">Contact Us</h5>
+                <p class="white-text" style="font-family: serif;">For queries contact us on following handles or call on<br>(+91)9354XXXX</p>
+              </div>
+              <div class="col l4 offset-l2 s12">
+               
+                <ul>
+                  <li><a class="fa fa-facebook" href="https://www.facebook.com/surbhi.garg.9022/">Facebook</a></li>
+                  <li><a class="fa fa-twitter" href=>Twitter</a></li>
+                  <li><a class="fa fa-instagram" href=>Instagram</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div class="footer-copyright">
+            <div class="container center" style="font-family: monospace;">
+              <div class="white-text">
+            Copyright ©2020 
+          </div>
+            </div>
+          </div>
+        </footer>
+            
+    </section>
---
+++ navbar.php
@@ -0,0 +1,16 @@
+<section>
+    <div class="navbar-fixed z-depth-2">
+        <nav class="navbar">
+            <div class="nav-wrapper black">
+                <a href="index.php" class="brand-logo"><img src="logo.jpg" height="40px" width="auto" style="border-radius: 50%" >THE SPARK FOUNDATION</a>
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="selectuser.php">Select User</a></li>
+                    <li><a href="viewUsers.php">View Users</a></li>
+                    <li><a href="transferHistory.php">Transfer History</a></li>
+                    <li><a href="aboutus.php">About us</a></li>
+                </ul>
+            </div>
+        </nav>
+    </div>
+
+    </section>
